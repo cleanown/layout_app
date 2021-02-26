616dd72f159712aefec3a8591b5d95964c4711be
cleanown <1346387492@qq.com>
2021-02-26 14:44:27 +0800

    1

---
+++ lib/pages/controlTest/controlTest.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class ControlTest extends StatefulWidget {
+  @override
+  _ControlTestState createState() => _ControlTestState();
+}
+
+class _ControlTestState extends State<ControlTest> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
